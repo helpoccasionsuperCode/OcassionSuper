67f7a6458bfff4102664599172f7b67a1bd5081e
ShashwatkumarXD <shashwat200418@gmail.com>
2025-09-07 11:27:05 +0530

    updated 9870823328

--- frontend/src/pages/VendorAuth.jsx
+++ frontend/src/pages/VendorAuth.jsx
@@ -201,7 +201,7 @@ import Register from "../components/VendorRegister/Register"
 import Login from "../components/Login"
 import Footer from "../components/Footer"
 // const adminPhone = import.meta.env.VITE_PHONE;
-const adminPhone = "9876543210";
+const adminPhone = "9870823328";
 
 const VendorReg = () => {
   const navigate = useNavigate();
